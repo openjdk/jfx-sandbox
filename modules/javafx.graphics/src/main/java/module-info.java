d95958a6821483ffe1d50504859d684231961a02
Johan Vos <johan@lodgon.com>
2024-11-14 14:05:55 +0100

    Add processing for keyinput

--- modules/javafx.graphics/src/main/java/module-info.java
+++ modules/javafx.graphics/src/main/java/module-info.java
@@ -65,6 +65,7 @@ module javafx.graphics {
     exports javafx.scene.text;
     exports javafx.scene.transform;
     exports javafx.stage;
+    exports com.sun.glass.ui.headless;
 
     exports com.sun.glass.ui to
         javafx.media,
