c3b3edb9eec39f35dede1fd82746f2d3b10d8150
Ambarish Rapte <arapte@openjdk.org>
2025-05-26 11:10:41 +0530

    Address review comments 1

--- modules/javafx.graphics/src/main/java/com/sun/prism/impl/PrismSettings.java
+++ modules/javafx.graphics/src/main/java/com/sun/prism/impl/PrismSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -208,7 +208,6 @@ public final class PrismSettings {
             if (PlatformUtil.isWindows()) {
                 tryOrderArr = new String[] { "d3d", "sw" };
             } else if (PlatformUtil.isMac()) {
-                // TODO: MTL: es2 will remain first in order for first few releases.
                 tryOrderArr = new String[] { "es2", "mtl", "sw" };
             } else if (PlatformUtil.isIOS()) {
                 tryOrderArr = new String[] { "es2" };
